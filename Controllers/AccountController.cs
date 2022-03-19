82486e6c450f59d2359aa5138d1aa1c09104c583
Pedro Matheus <matt_4.6.8@outlook.com>
2022-03-19 14:58:48 -0300

    Iniciando AccountController com metodo de registro de contas

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.ViewModels;
+using Blog.ViewModels.Accounts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("v1/accounts/")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model, [FromServices] BlogDataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email
+                }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(500, new ResultViewModel<string>("05XE4 - Este E-mail já está cadastrado"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("05X06 - Falha interna no servidor"));
+            }
+        }
+    }
+}
