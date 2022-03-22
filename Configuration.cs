70a0a39529c7447cc027e77f191f225324afeb16
Pedro Matheus <matt_4.6.8@outlook.com>
2022-03-22 05:50:06 -0300

    Implementado um ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,9 +2,9 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+        public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
         public static string ApiKeyName = "api_key";
-        public static string ApiKey = "";
+        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
         public static SmtpConfiguration Smtp = new SmtpConfiguration();
         public class SmtpConfiguration
         {
